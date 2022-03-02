b5c59e665263095b2e91faafe4641f0471ffef26
dabagirevalens <vdabagire@gmail.com>
2022-03-02 09:34:15 +0200

    first commit

---
+++ pages/Dashboard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import Header from '../components/Header'
+
+const Dashboard = ({ address }) => {
+    return (
+        <Wrapper>
+            <MainContainer>
+                <Header />
+            </MainContainer>
+        </Wrapper>
+    )
+}
+
+export default Dashboard
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    color: #fff;
+`
+
+const MainContainer = styled.div`
+    flex: 1;
+`
